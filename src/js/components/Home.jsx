f770f8fe9c01067ec95ed4437dfce8f1109e4166
Mario Daza <mariodaza525@gmail.com>
2026-04-24 13:51:03 +0200

    Todo list sin opcion de eliminar tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import Tasks from "./Tasks";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className=" d-flex flex-column text-center bg-secondary w-100 h-100">
+            <h1 className="text-white">To Do List</h1>
+			<Tasks/>
 		</div>
 	);
 };
---
+++ src/js/components/Tasks.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Tasks = () => {
+    const [list, setList] = useState([]);
+    const [input, setInput] = useState("");
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setList([...list, input])
+        }
+    };
+
+
+    return (
+        <div className="container bg-white text-secondary p-5">
+            <input type="text"
+                className="form-control border-0 w-100 shadow-none"
+                placeholder="What needs to be done?"
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            {list.length > 0 ?
+                <div className="pb-2 border-top">
+                    {list.map((e, i) => (
+
+                        <div key={i} className="d-flex align-items-center border-bottom w-100 task-row">
+                            <p  className="flex-grow-1 m-0 p-2 text-center " >{e}</p>
+                            <i className="fa-solid fa-xmark text-danger pe-3 icon-hidden"></i>
+                        </div>
+                    ))
+                    }
+                    <div className="text-start small">{list.length} items</div>
+                </div>
+                : <div className="fs-5 ">There are no tasks, add tasks.</div>}
+        </div>
+    )
+}
+
+
+export default Tasks;
